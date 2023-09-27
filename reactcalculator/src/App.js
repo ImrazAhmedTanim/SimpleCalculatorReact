c1a36b441124756fa5cb8fdf2338c376a5cb30a9
ImrazAhmedTanim <tanimf1@gmail.com>
2023-09-27 15:43:46 +0600

    reupload

--- reactcalculator/src/App.js
+++ reactcalculator/src/App.js
@@ -1,64 +1,84 @@
-import React,{useState,useRef} from 'react';
+import {useState,useRef} from 'react';
+
+
+
 function App() {
-
-  const useInput = useRef(null);
+  const firstInputRef = useRef(null);
+  const secondInputRef = useRef(null);
   const resultRef = useRef(null);
-  const [result,setResult] = useState(0);
-  const add = (e)=>{
-    e.preventDefault();
-    setResult((result)=>  result + Number(useInput.current.value));
-  };
-  const minus = (e)=>{
-    e.preventDefault();
-    const inputVal = useInput.current.value;
-    setResult(result => result -Number(inputVal));
-  
-  };
-  const multiply = (e)=>{
-    e.preventDefault();
-    const inputMul = useInput.current.value;
-    setResult(result => result * Number(inputMul));
-  };
-  const divide = (e)=>{
-    e.preventDefault();
-    const inputDiv = useInput.current.value;
-    setResult(result => result * Number(inputDiv));
-  };
-  const resetInput = (e)=> {
-    e.preventDefault();
-    useInput.current.value =0;
 
+  const [result, setResult] = useState(0);
+
+  const add = (e) => {
+    e.preventDefault();
+    const firstValue = Number(firstInputRef.current.value);
+    const secondValue = Number(secondInputRef.current.value);
+    setResult(firstValue + secondValue);
   };
-  const resetResult = (e)=>{
+
+  const subtract = (e) => {
+    e.preventDefault();
+    const firstValue = Number(firstInputRef.current.value);
+    const secondValue = Number(secondInputRef.current.value);
+    setResult(firstValue - secondValue);
+  };
+
+  const multiply = (e) => {
+    e.preventDefault();
+    const firstValue = Number(firstInputRef.current.value);
+    const secondValue = Number(secondInputRef.current.value);
+    setResult(firstValue * secondValue);
+  };
+
+  const divide = (e) => {
+    e.preventDefault();
+    const firstValue = Number(firstInputRef.current.value);
+    const secondValue = Number(secondInputRef.current.value);
+    setResult(firstValue / secondValue);
+  };
+
+  const resetInputs = (e) => {
+    e.preventDefault();
+    firstInputRef.current.value = '';
+    secondInputRef.current.value = '';
+  };
+
+  const resetResult = (e) => {
     e.preventDefault();
     setResult(0);
-  }
+  };
+
   return (
-     <>
-      <div> 
-        <h1>Simplest Working Calculator</h1> 
+    <>
+      <div>
+        <h1>Simple Calculator</h1>
       </div>
-      <form> 
-      <p ref={resultRef}>{result}</p> 
-      <input 
-        pattern="[0-9]" 
-        ref={useInput} 
-        type="number" 
-        placeholder="Type a number" 
-      /> 
-      
-      <button onClick={add}>add</button>
-      <button onClick={minus}>substract</button>
-      <button onClick={multiply}>multiply</button>
-      <button onClick={divide}>divide</button>
-
-
-      <button onClick={resetInput}>resetInput</button>
-      <button onClick={resetResult}>resetResult</button>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={firstInputRef}
+          type="number"
+          placeholder="Enter the first number"
+        />
+        <input
+          pattern="[0-9]"
+          ref={secondInputRef}
+          type="number"
+          placeholder="Enter the second number"
+        />
+        <button onClick={add}>Add</button>
+        <button onClick={subtract}>Subtract</button>
+        <button onClick={multiply}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInputs}>Reset Inputs</button>
+        <button onClick={resetResult}>Reset Result</button>
       </form>
     </>
-
   );
 }
 
 export default App;
+
+
+
